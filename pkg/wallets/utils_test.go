f9b3ae4bd530bc8b0ac7bd1572e857dc889e7b82
Planxnx <planxthanee@gmail.com>
2022-07-25 08:45:08 +0700

    tests: add bytes2string unittest

---
+++ pkg/wallets/utils_test.go
@@ -0,0 +1,39 @@
+package wallets
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestByteToString(t *testing.T) {
+	{
+		expectedBytes := []byte("hello world")
+		expected := string(expectedBytes)
+		actual := b2s(expectedBytes)
+		if expected != actual {
+			t.Errorf("expected: %s, actual: %s", expected, actual)
+		}
+	}
+	{
+		expectedBytes := make([]byte, 1024*1024)
+		if _, err := rand.Read(expectedBytes); err != nil {
+			t.Error(err)
+			t.FailNow()
+		}
+
+		expected := string(expectedBytes)
+		actual := b2s(expectedBytes)
+		if expected != actual {
+			t.Errorf("expected: %s, actual: %s", expected, actual)
+		}
+	}
+	{
+		expectedBytes := make([]byte, 1024*1024)
+		expected := string(expectedBytes)
+		actual := b2s(expectedBytes)
+		if expected != actual {
+			t.Errorf("expected: %s, actual: %s", expected, actual)
+		}
+	}
+
+}
