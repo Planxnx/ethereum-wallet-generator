65310993d8c148ff2d3ed11a02c7c734bbef159d
Planxnx <planxthanee@gmail.com>
2023-03-30 17:49:49 +0700

    fix: go sum  cann't lookup capital letter

--- internal/generators/generators.go
+++ internal/generators/generators.go
@@ -8,8 +8,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/Planxnx/ethereum-wallet-generator/pkg/progressbar"
-	"github.com/Planxnx/ethereum-wallet-generator/pkg/wallets"
+	"github.com/planxnx/ethereum-wallet-generator/pkg/progressbar"
+	"github.com/planxnx/ethereum-wallet-generator/pkg/wallets"
 )
 
 type Config struct {
