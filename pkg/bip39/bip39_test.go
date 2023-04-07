4352e0d5b41c3994706ce79a6ff127e13a2992c2
Planxnx <planxthanee@gmail.com>
2023-04-08 02:37:15 +0700

    fix(bip39): wrong err check in benchmark

--- pkg/bip39/bip39_test.go
+++ pkg/bip39/bip39_test.go
@@ -19,12 +19,12 @@ func BenchmarkBIP39(b *testing.B) {
 	for name, generator := range generators {
 		for _, bitSize := range bitSizes {
 			b.Run(fmt.Sprintf("%s:%d", name, bitSize), func(b *testing.B) {
-				entropy, err := bip39.NewEntropy(bitSize)
 				b.ResetTimer()
+				entropy, err := bip39.NewEntropy(bitSize)
+				if err != nil {
+					b.Fatal(err)
+				}
 				for i := 0; i < b.N; i++ {
-					if err != nil {
-						b.Fatal(err)
-					}
 					_, _ = generator(entropy)
 				}
 			})
